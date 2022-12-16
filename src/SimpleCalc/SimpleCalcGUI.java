9430039bf3cef0873c5e7ccc728d9d2c24bcfa32
Josh Dimpas <40543984+JoshCunningHum@users.noreply.github.com>
2022-12-16 15:50:07 +0800

    Add exception handling to SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,8 +1,7 @@
 package SimpleCalc;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.regex.Pattern;
 
 public class SimpleCalcGUI extends JFrame{
     private JPanel main;
@@ -21,28 +20,41 @@ public class SimpleCalcGUI extends JFrame{
 
         lblResult.setEditable(false);
 
-        btnCompute.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                compute();
-            }
-        });
+        btnCompute.addActionListener(e -> compute());
     }
 
     private void compute(){
-        double num1 = Double.parseDouble(tfNumber1.getText());
-        double num2 = Double.parseDouble(tfNumber2.getText());
+        double num1 = 0, num2;
 
-        int op = cbOperations.getSelectedIndex();
-        double result = switch (op) {
-            case 0 -> num1 + num2;
-            case 1 -> num1 - num2;
-            case 2 -> num1 * num2;
-            case 3 -> num1 / num2;
-            default -> 0;
-        };
+        try {
 
-        lblResult.setText(String.format("%.0f", result));
+            num1 = Double.parseDouble(tfNumber1.getText());
+            num2 = Double.parseDouble(tfNumber2.getText());
+
+            int op = cbOperations.getSelectedIndex();
+
+            if(num2 == 0 && op == 3) throw new ArithmeticException("Divide by zero");
+
+            double result = switch (op) {
+                case 0 -> num1 + num2;
+                case 1 -> num1 - num2;
+                case 2 -> num1 * num2;
+                case 3 -> num1 / num2;
+                default -> 0;
+            };
+
+            lblResult.setText(String.format("%.0f", result));
+        }catch (NumberFormatException e){
+            JOptionPane.showMessageDialog(this, "Please input a number");
+
+            if(num1 == 0) tfNumber1.setText("");
+            if(Pattern.matches(".*\\D+.*", tfNumber2.getText())) tfNumber2.setText("");
+        }catch (ArithmeticException e){
+            JOptionPane.showMessageDialog(this, "Can't divide by zero!");
+            tfNumber2.setText("");
+        }catch (Exception ignored){
+
+        }
     }
     public static void main(String[] args){
         JFrame UI = new SimpleCalcGUI();
